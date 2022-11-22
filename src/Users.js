0262deebb6af669591b4df6aae6c3ef82edd4578
Jan Silva <janfrs3@gmail.com>
2022-11-22 22:17:50 +0100

    Update Users route to fetch users

--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Table from 'react-bootstrap/Table';
 
 export default function Users() {
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        let url="https://jsonplaceholder.typicode.com/users"
+        fetch(url).then((response)=>{
+            response.json().then((result)=>{
+                setData(result)
+            })
+        })
+    },[])
     return (
-        <div><h1>Users component</h1></div>
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data.map((item) => (
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.email}</td>
+                            <td>{item.address.street}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </Table>
     )
 }
